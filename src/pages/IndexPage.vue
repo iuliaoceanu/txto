f818081acbb6ee213ddfcea8ccdaa84239e69ba2
cristian <electronrecord@gmail.com>
2024-04-26 11:24:31 +0300

    added component structure

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,11 +1,33 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
-  </q-page>
+  <main class="home-page">
+    <MainHeader />
+
+    <HeroBanner />
+
+    <GamePresentation />
+
+    <TxtoPackage />
+
+    <TxtoGames />
+
+    <WhyToPlay />
+
+    <Testimonials />
+
+    <WhatsInside />
+
+    <FAQ />
+
+    <PriceDetails />
+
+    <WhoCreatedTxto />
+
+    <TxtoCommunity />
+
+    <Contact />
+
+    <MainFooter />
+  </main>
 </template>
 
 <script setup>
